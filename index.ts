e42b7f5f0647db2a5ba4f270721049164aed32a1
Simon Schwedes <simon@schwedes.eu>
2023-01-23 21:25:40 +0100

    continued with day 7

--- index.ts
+++ index.ts
@@ -12,11 +12,13 @@ import run6_1 from "./days/6/1";
 import run6_2 from "./days/6/2";
 import run7_1 from "./days/7/1";
 import run7_2 from "./days/7/2";
+import run8_1 from "./days/8/1";
+import run8_2 from "./days/8/2";
 
-const day = 7;
-const part = 2;
+const day = 8;
+const part = 1;
 console.log("Hello via Bun!");
 console.log("Running advend of code 2022!!! <3")
 console.log(`Current day: ${day} and part: ${part}`);
 
-console.log(await run7_2());
+console.log(await run8_1());
